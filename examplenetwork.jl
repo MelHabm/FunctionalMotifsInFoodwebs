06e988fc2183895ee0a43c3e3ca2c73d67518be1
MelHabm <mel2.habermann@gmail.com>
2025-08-17 15:43:14 +0200

    Update to examplenetwork.jl
    Signed-off-by: MelHabm <mel2.habermann@gmail.com>

--- examplenetwork.jl
+++ examplenetwork.jl
@@ -17,15 +17,31 @@ interactions = Matrix{Int64}(df)
 # Initializing ------------------------------------------
 # Number of iterations (determines the number of different parametrizations or communities)
 n = 10^4;
+# Number of species
+N = 15;
 
 # Find and analyze motifs -------------------------------
 # Note: Version right now checks for all motifs. Future updates will include motifs to be picked individually
-results = FunctionalMotifs.func_react_motifs(n, interactions)
+results = FunctionalMotifs.func_react_motifs(n, interactions);
+
+# Repeat process for topologies made with the niche model
+results_niche = FunctionalMotifs.react_motifs_niche(n, N);
+
+# Clean data --------------------------------------------
+# Necessary step because the niche model sometimes produces topologies where we don't find all motifs. 
+# Empty values have to be removed for the plot
+cl_prop_pp = filter(!=(-999), results_niche.props[:pp])
+cl_prop_ac = filter(!=(-999), results_niche.props[:ac])
+cl_prop_ec = filter(!=(-999), results_niche.props[:ec])
+cl_prop_ch3 = filter(!=(-999), results_niche.props[:ch3])
 
 # Create histograms -------------------------------------
-ReactivityResults.hist_probdens2x2([
-    (data = results.props[:pp], title = "Predator-Prey"),
-    (data = results.props[:ac], title = "Apparent Competition"),
-    (data = results.props[:ec], title = "Exploitative Competition"),
-    (data = results.props[:ch3], title = "Tri-Trophic Chain"),
-])
+ReactivityResults.hist_probdens2x2_comp([
+    (data1 = cl_prop_pp, data2 = results.props[:pp], title = "Predator-Prey"),
+    (data1 = cl_prop_ac, data2 = results.props[:ac], title = "Apparent Competition"),
+    (data1 = cl_prop_ec, data2 = results.props[:ec], title = "Exploitative Competition"),
+    (data1 = cl_prop_ch3, data2 = results.props[:ch3], title = "Tri-Trophic Chain")
+],
+maxval = 6.5,
+outpath = "comparison.png",
+leg_labels = ("Different Topologies", "Example Network"))
